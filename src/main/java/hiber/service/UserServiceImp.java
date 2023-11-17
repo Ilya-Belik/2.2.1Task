95a5e08239666d2ab7784a7a101b322e25ecfc29
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2023-11-17 22:33:59 +0500

    Add files via upload

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -1,6 +1,8 @@
 package hiber.service;
+
 import hiber.dao.UserDao;
 import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,11 +11,8 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   private final UserDao userDao;
-
-   public UserServiceImp(UserDao userDao) {
-      this.userDao = userDao;
-   }
+   @Autowired
+   private UserDao userDao;
 
    @Transactional
    @Override
@@ -27,4 +26,15 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional
+   @Override
+   public void deleteAllUsers(){
+      userDao.deleteAllUsers();
+   }
+
+   @Transactional
+   @Override
+   public User findOwner(String car_name, String car_series) {
+      return userDao.findOwner(car_name, car_series);
+   }
 }
